324f10a93a6d5779d520cd8b52cd83c02d452388
AveNRU <ad.avenger@gmail.com>
2026-09-12 01:39:15 +0300

    +

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use chrono::*;
 //use unirust::*;
 //use std::collections::HashMap;
 use cap::Cap;
+use gpui_kit::component::button::*;
+use gpui_kit::component::*;
+use gpui_kit::*;
 use std::alloc;
 use std::env;
 use std::time::{
@@ -29,7 +32,24 @@ use crate::output::write;
 use crate::utils::functions_add::system_pause;
 use console::style;
 //use rayon::scope;
-
+pub struct HelloWorld;
+impl Render for HelloWorld {
+    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .v_flex()
+            .gap_2()
+            .size_full()
+            .items_center()
+            .justify_center()
+            .child("Hello, World!")
+            .child(
+                Button::new("ok")
+                    .primary()
+                    .label("Let's Go!")
+                    .on_click(|_, _, _| println!("Clicked!")),
+            )
+    }
+}
 #[global_allocator]
 static ALLOCATOR: Cap<alloc::System> = Cap::new(alloc::System, usize::max_value());
 //use std::thread;
@@ -53,6 +73,23 @@ async fn main() {
         исполнение,
         текущая_время_дата.format("%d-%m-%Y время: %H:%M:%S")
     );
+    //
+    /*gpui_kit::application().run(move |cx| {
+        // This must be called before using any GPUI Component features.
+        gpui_kit::init(cx);
+
+        cx.spawn(async move |cx| {
+            cx.open_window(WindowOptions::default(), |window, cx| {
+                let view = cx.new(|_| HelloWorld);
+                // This first level on the window, should be a Root.
+                cx.new(|cx| Root::new(view, window, cx))
+            })
+            .expect("Failed to open window");
+        })
+        .detach();
+    });
+    system_pause();*/
+    //
     let mut сообщения: Text_Changer::Сообщения = Default::default();
     //подсчёт начала запуска времени
     //начало нового
